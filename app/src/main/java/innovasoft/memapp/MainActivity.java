8a23d416e75861bdd58b77cc04f8cfdd2d663b64
Cj12l <carlos.jimenez7@correo.icesi.edu.co>
2015-10-22 19:53:04 -0500

    prueba

--- app/src/main/java/innovasoft/memapp/MainActivity.java
+++ app/src/main/java/innovasoft/memapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private String n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
